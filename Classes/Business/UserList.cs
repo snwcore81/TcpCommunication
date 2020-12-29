f5ee796a018b3fd2754f40504b9afc100dbee193
Jacek Kuźmicz <snwcore@gmail.com>
2020-12-29 10:46:30 +0100

    *** UserList collection ***

---
+++ Classes/Business/UserList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using TcpCommunication.Classes.System;
+
+namespace TcpCommunication.Classes.Business
+{
+    [DataContract]
+    public class UserList : XmlStorage<UserList>
+    {
+        [DataMember]
+        private List<User> UserCollection { get; set; }
+
+        public UserList()
+        {
+            UserCollection = new List<User>();
+        }
+
+        public override bool InitializeFromObject(UserList Object)
+        {
+            this.UserCollection = new List<User>(Object.UserCollection);
+
+            return true;
+        }
+
+        public virtual void SaveAsXml(string a_sFileName)
+        {
+            using var _log = Log.DEB(this, "SaveAsXml");
+
+            try
+            {
+                using (var _file = new StreamWriter(a_sFileName))
+                {
+                    var _sStrBuff = Encoding.UTF8.GetString(ToXml().ToArray());
+
+                    _log.PR_DEB(_sStrBuff);
+
+                    _file.Write(_sStrBuff);
+                }
+            }
+            catch (Exception e)
+            {
+                _log.PR_DEB($"Błąd! {e.Message}");
+            }
+        }
+
+        public virtual void LoadFromXml(string a_sFileName)
+        {
+            using var _log = Log.DEB(this, "LoadFromXml");
+
+            try
+            {
+                _log.PR_DEB($"próba odczytu użytkowników z pliku <{a_sFileName}>...");
+
+                using (var _file = new StreamReader(a_sFileName))
+                {
+                    var _sStrBuff = _file.ReadToEnd();
+
+                    var _oBuffer = Encoding.UTF8.GetBytes(_sStrBuff);
+
+                    var _oUserList = new UserList();
+
+                    _oUserList.FromXml(new MemoryStream(_oBuffer));
+
+                    this.InitializeFromObject(_oUserList);
+
+                }
+            }
+            catch (Exception e)
+            {
+                _log.PR_DEB($"Błąd! {e.Message}");
+            }
+        }
+
+        public virtual User Add(User a_oUser)
+        {
+            UserCollection.Add(a_oUser);
+
+            return a_oUser;
+        }
+        public virtual User Add() => Add(User.Add());
+        public virtual User Get(int a_iIndex) => UserCollection[a_iIndex];
+        public virtual int Count => UserCollection.Count;
+
+        public List<User> Collection => UserCollection;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -23,61 +23,82 @@ namespace TcpCommunication
         {
             Console.Clear();
 
-            //User.Add().SaveAsXml("user.xml");
+            UserList _oLista = new UserList();
+
+            if (File.Exists(@"baza_uzytkownikow.xml"))
+            {
+                _oLista.LoadFromXml(@"baza_uzytkownikow.xml");
+            }
+            else
+            {
+                _oLista.Add(new User { Login = "jkuzmicz", Password = "test123", Permission = 2 });
+                _oLista.Add(new User { Login = "zdyrman", Password = "test123", Permission = 1 });
+                _oLista.Add(new User { Login = "rochucki", Password = "test123", Permission = 2 });
+
+                _oLista.SaveAsXml(@"baza_uzytkownikow.xml");
+            }
+
+            foreach (var _user in _oLista.Collection)
+            {
+                Console.WriteLine(_user);
+            }
+
+            /*
+            User.Add().SaveAsXml("user.xml");
 
             var _oUser = User.LoadFromXml(@"user.xml");
 
             Console.WriteLine(_oUser);
-
-            /*
-            Log.CurrentLevel = Log.LevelEnum.DEB;
-            Log.LogWriter = new ConsoleLogWriter();
-
-            using var _log = Log.DEB("Program", "Main");
-
-            var _db = new MySqlSource
-            {
-                Host = "127.0.0.1",
-                Port = 3306,
-                Login = "admin",
-                Password = "mydatabase1234",
-                Schema = "mydatabase"
-            };
-
-            _db.Connect();
-
-
-
-            foreach (var _oLogin in _db.ExecuteReader<LoginDbObject>())
-            {
-                try
-                {
-                    _db.TransactionStart();
-
-                    Console.WriteLine(_oLogin);
-
-                    Console.Write($"Podaj nowe hasło dla użytkownika <{_oLogin.Login}>:");
-                    _oLogin.Password = Console.ReadLine();
-
-                    if (_oLogin.IsChanged)
-                    {
-                        _oLogin.LastUpdate = DateTime.Now;
-                        _oLogin.Update(_db);
-                    }
-
-                    _db.TransactionCommit();
-
-                }
-                catch (Exception e)
-                {
-                    _log.PR_DEB($"Wyjątek! {e.Message}");
-
-                    _db.TransactionRollback();
-                }
-            }
-            
-            _db.Disconnect();
             */
+                /*
+                Log.CurrentLevel = Log.LevelEnum.DEB;
+                Log.LogWriter = new ConsoleLogWriter();
+
+                using var _log = Log.DEB("Program", "Main");
+
+                var _db = new MySqlSource
+                {
+                    Host = "127.0.0.1",
+                    Port = 3306,
+                    Login = "admin",
+                    Password = "mydatabase1234",
+                    Schema = "mydatabase"
+                };
+
+                _db.Connect();
+
+
+
+                foreach (var _oLogin in _db.ExecuteReader<LoginDbObject>())
+                {
+                    try
+                    {
+                        _db.TransactionStart();
+
+                        Console.WriteLine(_oLogin);
+
+                        Console.Write($"Podaj nowe hasło dla użytkownika <{_oLogin.Login}>:");
+                        _oLogin.Password = Console.ReadLine();
+
+                        if (_oLogin.IsChanged)
+                        {
+                            _oLogin.LastUpdate = DateTime.Now;
+                            _oLogin.Update(_db);
+                        }
+
+                        _db.TransactionCommit();
+
+                    }
+                    catch (Exception e)
+                    {
+                        _log.PR_DEB($"Wyjątek! {e.Message}");
+
+                        _db.TransactionRollback();
+                    }
+                }
+
+                _db.Disconnect();
+                */
         }
     }
 }
